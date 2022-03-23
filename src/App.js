80597524f3aab6bd0bb0922c428ad77d299d3d73
王鹤垚 <wangheyao@zealfi.com>
2022-03-23 16:31:28 +0800

    fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Button} from "antd";
+import "./style/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button type="primary">Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
